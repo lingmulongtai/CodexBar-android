c50d8a30a8e74b104e12bc1648bed4599f15078f
Ryuta <lingmulongtai200@gmail.com>
2026-07-13 04:15:31 +0900

    release: bump version to 0.1.0-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.codexbar.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.0.3-beta"
+        versionCode = 6
+        versionName = "0.1.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
