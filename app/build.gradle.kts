d7dce2c4e144adef209f62dbcea5626678862289
Ryuta <lingmulongtai200@gmail.com>
2026-07-14 11:47:44 +0900

    chore: prepare v0.3.1 stable release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.codexbar.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.3.0"
+        versionCode = 11
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
