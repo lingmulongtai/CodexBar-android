d601445d8c505d4f9dbfb8368afb0aca01afbd73
Ryuta <lingmulongtai200@gmail.com>
2026-07-14 14:54:40 +0900

    chore: prepare v0.3.4 stable release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.codexbar.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "0.3.3"
+        versionCode = 14
+        versionName = "0.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
